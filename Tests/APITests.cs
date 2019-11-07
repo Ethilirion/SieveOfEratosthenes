f7539b60e5f7ea2774d7f706baa8b0f8aec5f6ed
Ethilirion <pierre.hamelin@epitech.eu>
2019-11-07 14:44:31 +0100

    premier test api

---
+++ Tests/APITests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using API_Controllers = API.Controllers;
+using Xunit;
+using System.Linq;
+
+namespace Tests
+{
+    public class APITests : IDisposable
+    {
+        private API_Controllers.Sieve sieveAPI;
+
+        public APITests()
+        {
+            sieveAPI = new API_Controllers.Sieve();
+        }
+
+        [Theory]
+        [InlineData(656578)]
+        [InlineData(2)]
+        [InlineData(80)]
+        public void GetResultsFromValidInput(uint number)
+        {
+            var results = sieveAPI.GetSieveListOfResult(number)?.Value;
+            Assert.True(results.Any(prime => prime == 0) == false);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
